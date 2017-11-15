b7bc47fe7bdbd67915d880218d0f3a29f6db02ad
Busheng Xi <busheng.xi@originenergy.com.au>
2017-11-15 12:23:39 +1100

    Hello my.js

---
+++ src/my.js
@@ -0,0 +1,73 @@
+( function( my ) {
+
+    if ( my !== undefined ) {
+
+        window.alert( 'window.my is defined!' );
+        return;
+    }
+    else {
+
+        window.my = {};
+    }
+
+
+    /* Poll until condition is met
+     *
+     * @param { function } conditionCallback - function returns true or false
+     * @param { number } interval - poll every `interval` seconds, default 100
+     * @param { number } max - maximum times it polls, default: 50
+     */
+    function pollWithCondition( conditionCallback, onConditionMet, interval, max ) {
+
+        var interval = ( typeof interval === 'number' ? interval : 100 );
+        var max = ( typeof max === 'number' ? max : 50 );
+
+        var count = 0;
+
+        function poll() {
+
+            if ( conditionCallback() === true ) {
+
+                onConditionMet();
+            }
+            else {
+
+                if ( count < max ) {
+
+                    setTimeout( poll, interval );
+                    count ++;
+                }
+            }
+        }
+
+        poll();
+    }
+
+    /* Exmample: ifDefined( window.jQuery, window._ )
+     *
+     */
+    function ifDefined() {
+
+        for ( var i = 0; i < arguments.length; i ++ ) {
+
+            var obj = arguments[ i ];
+
+            if ( obj === undefined ) {
+
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    // Assignment
+    window.my = {
+
+        if: pollWithCondition,
+        ifDefined: ifDefined
+    };
+
+
+} )( window.my );
