02ba54863687d7afe320127a9f3fcfbb5fae7d11
Busheng Xi <busheng.xi@yvw.com.au>
2018-09-25 09:31:03 +1000

    Added more functions

--- src/my.js
+++ src/my.js
@@ -21,9 +21,8 @@
      *
      * @param { function } conditionCallback - Function returns true or false
      * @param { function } onConditionMet - Run when conditionCallback returns true
-     * @param { number } interval - Poll every `interval` seconds, default to 100
+     * @param { number } interval - Poll every interval seconds, default to 100
      * @param { number } max - Maximum trials of polling, default to 50
-     * @returns { undefined } Undefined
      *
      * @memberof my
      */
@@ -69,6 +68,184 @@
         }
 
         return true;
+    }
+
+    /**
+     * Check if it is an object like { a: 1, b: 2 }, not function, null, array, etc 
+     *
+     * @memberof my
+     * @link https://stackoverflow.com/questions/8834126/how-to-efficiently-check-if-variable-is-array-or-object-in-nodejs-v8
+     */
+    function isObject( a ) {
+
+        return ( !!a ) && ( a.constructor === Object );
+    };
+
+    /** 
+     * Generate a string contains random characters
+     *
+     * @returns { string } Random string
+     * @memberof my
+     */
+    function generateRandomString() {
+
+        return Math.random().toString( 36 ).slice( 2 );
+    }
+
+    /**  
+     * Set a variable with a value, if the default value is undefined
+     * 
+     * @returns { any } Either the defaultValue or the otherValue
+     * @memberof my
+     */
+    function setDefault( defaultValue, otherValue ) {
+        
+        if ( defaultValue === undefined ) {
+            
+            return otherValue;
+        }
+        
+        return defaultValue;
+    }
+
+    /**
+     * Check a DOM element is the descendant of another element
+     *
+     * @memberof my
+     */
+    function isDescendant( childElem, parentElem ) {
+        
+        let node = childElem.parentNode;
+        
+        while ( node !== null && node.parentNode !== undefined ) {
+            
+            if ( node === parentElem ) {
+
+                return true;
+            }
+            
+            node = node.parentNode;
+        }
+        
+        return false;
+    }
+
+    /** 
+     * Check if a variable is an array and must have at least 1 item.
+     *
+     * @memberof my
+     */
+    function isNotEmptyArray( arg ) {
+
+        return Array.isArray( arg ) && arg.length > 0;
+    }
+
+    /**
+     * Compare two arrays whose items are not objects
+     *
+     * @returns {boolean} True if two arrays are same, otherwise false.
+     * @memberof my
+     *
+     */
+    function compareArrayOfNonObjects( array1, array2 ) {
+
+        if ( Array.isArray( array1 ) === false || Array.isArray( array2 ) === false ) {
+
+            throw new TypeError( 'Both arguements should be arrays.' );
+        }
+        else if ( array1.length !== array2.length ) {
+
+            return false;
+        }
+
+        for ( let i = 0 ; i < array1.length ; i ++ ) {
+
+            let itemOfArray1 = array1[ i ];
+            let itemOfArray2 = array2[ i ];
+
+            if ( typeof itemOfArray1 === 'number'
+                    && typeof itemOfArray2 === 'number'
+                    && isNaN( itemOfArray1 ) === true 
+                    && isNaN( itemOfArray2 ) === true ) {
+
+                continue;
+            }
+            else if ( itemOfArray1 === null || itemOfArray2 === null ) {
+
+                // Do nothing
+            }
+            else if ( typeof itemOfArray1 === 'object' || typeof itemOfArray2 === 'object' ) {
+
+                throw new Error( 'This function should not contain an object or an array.' );
+            }
+
+            if ( itemOfArray1 !== itemOfArray2 ) {
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Given an array, find if it is an item in another array and return the index.
+     *
+     * @returns {number} If not found, return -1
+     * @memberof my
+     */
+    function findIndexFromArrayOfArray( item, arrayOfArray ) {
+
+        if ( Array.isArray( arrayOfArray ) === false ) {
+
+            throw new TypeError( '2nd argument should be an array.' );
+        }
+        else if ( Array.isArray( item ) === false ) {
+
+            return -1;
+        }
+
+        for ( let i = 0; i < arrayOfArray.length; i ++ ) {
+
+            let array = arrayOfArray[ i ];
+
+            try { 
+
+                if ( compareArrayOfNonObjects( item, array ) === true ) {
+
+                    return i;
+                }
+            }
+            catch ( error ) {
+
+                return -1;
+            }
+
+        }
+
+        return -1;
+    }
+
+    /**
+     * Get the union results of two arrays
+     *
+     * @memberof my
+     */
+    function unionArrayOfArray( array1, array2 ) {
+
+        let union = array2.slice();
+
+        for ( let eachArray of array1 ) {
+
+            let index = findIndexFromArrayOfArray( eachArray, array2 );
+
+            if ( index === -1 ) {
+
+                union.push( eachArray.slice() );
+            }
+        }
+
+        return union;
     }
 
     // Assignment
